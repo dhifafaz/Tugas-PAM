b13d16ed326e4065e77d3402a355566970792ba9
dhifafaz <dhifaf.az@gmail.com>
2022-03-08 21:48:03 +0700

    use state editor

--- healingid/src/components/main-form/MainForm.js
+++ healingid/src/components/main-form/MainForm.js
@@ -1,4 +1,5 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 import {
     SafeAreaView,
     ScrollView,
@@ -13,7 +14,7 @@ import formStyle from './MainFormStyle';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const MainForm = () => {
-    const [text, onChangeText] = React.useState({
+    const [text, onChangeText] = useState({
         asal: '',
         tujuan: '',
         tanggal: '',
