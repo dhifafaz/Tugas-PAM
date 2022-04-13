e1164d5d104fb793f503de97846215974fe9a84f
dhifafaz <dhifaf.az@gmail.com>
2022-04-13 16:04:00 +0700

    udah

---
+++ kapalan/src/components/BottomBar/LainnyaScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+
+    Text,
+    View,
+
+} from 'react-native';
+
+
+const LainnyaScreen = (props) => {
+    return (
+        <View>
+            <Text>
+                Lainnya
+            </Text>
+        </View>
+    );
+};
+
+export default LainnyaScreen;
